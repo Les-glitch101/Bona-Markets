7581c14ff3680b5e5b7310cb1fc5758fe81994a5
sharp-gabrielle <gabrielle.chloe.sharp@gmail.com>
2026-06-21 17:57:36 +0200

    Delete Database/prisma directory

--- Database/prisma/migrations/20260604100527_update_products_name_uniqueness/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,vendorId]` on the table `Products` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Products_name_vendorId_key" ON "Products"("name", "vendorId");
